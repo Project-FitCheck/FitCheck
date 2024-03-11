03d0c9625e1234ae78804e2ab4df5b3121e00cf4
Andy-Nguyen4 <andynguyen1541@gmail.com>
2024-03-11 16:20:07 -0400

    Minor changes to profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Switch from '@mui/material/Switch';
 import Select from 'react-select';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 const options = [
   {value: 'light', label: 'Light (Default)'},
@@ -47,9 +49,25 @@ function App() {
       </table>
     </div>
 
-    <div className="editModel">
+    <div className="edit">
         <button class="editButton">Edit Model</button>
-        <button class="password">Change Password</button>
+        <Popup trigger= {<button class="password">Change Password</button>}
+        modal nested> { 
+          close => (
+            <div class="modal">
+              <div class="oldPass">
+                Old Password
+              </div>
+              <div class="newPass">
+                New Password
+              </div>
+              <div>
+                <button onclick = {() => close()}>Submit</button>
+              </div>
+            </div>
+          )
+        }
+        </Popup>
     </div>
 
     <div className="themes">
