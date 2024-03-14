2e673d1aad1630334cdb2e9ec35e52c412c9c103
Ephraim Acquah <ephraimacquah14@gmail.com>
2024-03-14 16:55:20 -0400

    worked on closet api endpoints

--- backend/routes/closet.js
+++ backend/routes/closet.js
@@ -1,10 +1,17 @@
+/* eslint-disable no-unused-vars */
 import express from "express";
-import mongoose from "mongoose";
-import {ClosetModel} from "../models/Closets"
+import { ClosetModel } from "../models/Closets";
 
 const router = express.Router();
+/*req: {
+    user: closet_id
+}
+*/
 router.get("/", async (req, res) => {
-
+    const closetId = req.body;
+    const closet = await ClosetModel.findById({closetId});
+    const clothes = await closet.get("clothes");
+    res.json({clothes});
 });
 
 router.post("/add", async (req, res) => {
