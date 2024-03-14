3fab97916a1dc824b147e26263ba86a2c7757d95
Ephraim Acquah <103010587+ephraimacquah@users.noreply.github.com>
2024-03-14 15:24:46 -0400

    Lint (#21)
    * created routes folder and files
    
    * linted frontend and backend files
    
    * more linting

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose")
+import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
     firstName: {
@@ -37,5 +37,5 @@ const UserSchema = new mongoose.Schema({
 });
 
 const UserModel = mongoose.model("User", UserSchema);
-module.exports = UserModel
+export {UserModel};
 
