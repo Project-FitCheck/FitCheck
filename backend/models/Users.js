84c8bf9468db83da347544f5dd429b82c62f63e6
Ephraim Acquah <ephraimacquah14@gmail.com>
2024-03-14 21:24:01 -0400

    worked on model endpoints

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -22,7 +22,30 @@ const UserSchema = new mongoose.Schema({
         required: true
     },
     model: {
-        type: mongoose.Types.Buffer
+        gender: {
+            type: String
+        },
+        head: {
+            type: String
+        },
+        leftArm: {
+            type: String
+        },
+        rightArm: {
+            type: String
+        },
+        torso: {
+            type: String
+        },
+        legs: {
+            type: String
+        },
+        feet: {
+            type: String
+        },
+        fullBody: {
+            type: String
+        },
     },
     closet: {
         type: mongoose.Types.ObjectId,
--- backend/routes/closet.js
+++ backend/routes/closet.js
@@ -3,22 +3,43 @@ import express from "express";
 import { ClosetModel } from "../models/Closets";
 
 const router = express.Router();
+
 /*req: {
     user: closet_id
 }
 */
-router.get("/", async (req, res) => {
+router.get("/closet", async (req, res) => {
     const closetId = req.body;
     const closet = await ClosetModel.findById({ closetId });
     const clothes = await closet.get("clothes");
     res.json({ clothes });
 });
 
-router.post("/add", async (req, res) => {
+/*req: {
+    closetid: closet_id
+    clothing_item: {
+        productTitle: { type: String, required: true },
+        type: { type: String, required: true },
+        color: String,
+        size: String,
+        description: String,
+        material: String,
+        brand: String,
+        price: Number,
+        fit: String,
+        style: String
+    }
+}
+*/
+router.post("/closet/add", async (req, res) => {
 
 });
 
-router.post("/favorite", async (req, res) => {
+/*req: {
+    user: closet_id
+}
+*/
+router.post("/closet", async (req, res) => {
 
 });
 
