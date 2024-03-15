c9e2243c7e5effa0838c88b9683e51560996e016
Andy-Nguyen4 <andynguyen1541@gmail.com>
2024-03-15 12:39:01 -0400

    some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'reactjs-popup/dist/index.css';
 import Profile from "./pages/profile"
+import './App.css';
 
 function App() {
   return (
@@ -9,3 +10,7 @@ function App() {
 }
 
 export default App;
+
+
+
+
