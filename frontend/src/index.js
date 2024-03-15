d9963d7e1f0711a34a9e7ef8072a49fca71e23e1
Andy-Nguyen4 <andynguyen1541@gmail.com>
2024-03-15 12:36:57 -0400

    some changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './pages/profile';
 import reportWebVitals from './reportWebVitals';
 import {createBrowserRouter, BrowserRouter, } from "react-router-dom";
 
