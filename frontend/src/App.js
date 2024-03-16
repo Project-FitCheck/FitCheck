940744818ba535362d1a41b948ae7b00f6f8761f
Ephraim Acquah <ephraimacquah14@gmail.com>
2024-03-15 21:25:22 -0400

    set up the broswer router in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/closet' element={<closet/>}/>
+          <Route path='/closet/add' element={<></>}/>
+          <Route path='/model/edit' element={<model/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          {/*<Route path='/' element={<></>}/>*/}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
