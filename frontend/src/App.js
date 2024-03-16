3afddd90aba47149b16e1923f41fab8d4a4f2cbd
Andy-Nguyen4 <andynguyen1541@gmail.com>
2024-03-16 19:20:50 -0400

    created navbar and profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import 'reactjs-popup/dist/index.css';
-import Profile from "./pages/profile"
-import './App.css';
-import React from 'react';
-import 'reactjs-popup/dist/index.css';
-import Profile from "./pages/profile"
+import Profile from "./pages/profile";
+import './index.css';
+import NavBar from './components/navbar';
 
 function App() {
   return (
-    <Profile />
+
+    <div className="App">
+      <NavBar />
+      <Profile />
+    </div>
+    
   );
 }
 
--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -3,86 +3,82 @@ import Switch from '@mui/material/Switch';
 import Select from 'react-select';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
-import NavBar from '../components/navbar';
 
 const options = [
-  {value: 'light', label: 'Light (Default)'},
-  {value: 'dark', label: 'Dark'},
-  {value: 'midnight', label: 'Midnight'}
+  { value: 'light', label: 'Light (Default)' },
+  { value: 'dark', label: 'Dark' },
+  { value: 'midnight', label: 'Midnight' }
 ]
 
 function Profile() {
   return (
-    <>
-    <div className="navigation">
-      <NavBar />
-    </div>
 
-    <div className="settings">
-      <h1>SETTINGS</h1>
-    </div>
+    <div className="profile">
+      <div className="settings">
+        <h1>SETTINGS</h1>
+      </div>
 
-    <div className="account">
-      <h1>ACCOUNT INFORMATION</h1>
-    </div>
+      <div className="account">
+        <h1>ACCOUNT INFORMATION</h1>
+      </div>
 
-    <div className="profileImg">
-      <p>Profile image goes here</p>
-    </div>
+      <div className="profileImg">
+        <p>Profile image goes here</p>
+      </div>
 
-    <div className="info">
-      <table>
-        <tbody>
-          <tr>
-            <td>Name:</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Username:</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>DOB:</td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+      <div className="info">
+        <table>
+          <tbody>
+            <tr>
+              <td>Name:</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Username:</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>DOB:</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-    <div className="edit">
-      <button className="editButton">Edit Model</button>
-      <Popup trigger= {<button className="password">Change Password</button>}
-        modal nested> { 
-          close => (
-            <div className="modal">
-              <div className="oldPass">
-                <h1>Old Password</h1>
+      <div className="edit">
+        <button className="editButton">Edit Model</button>
+        <Popup trigger={<button className="password">Change Password</button>}
+          modal nested> {
+            close => (
+              <div className="modal">
+                <div className="oldPass">
+                  <h1>Old Password</h1>
+                </div>
+                <div className="newPass">
+                  <h1>New Password</h1>
+                </div>
+                <div>
+                  <button onclick={() => close()}>Submit</button>
+                </div>
               </div>
-              <div className="newPass">
-                <h1>New Password</h1>
-              </div>
-              <div>
-                <button onclick = {() => close()}>Submit</button>
-              </div>
-            </div>
-          )
-        }
-    </Popup>
+            )
+          }
+        </Popup>
+      </div>
+
+      <div className="themes">
+        <h2>THEMES</h2>
+        <p>Customize the theme of your FitCheck</p>
+        <Select options={options} />
+      </div>
+
+      <div className="privacy">
+        <h2>PRIVACY</h2>
+        <p>Make your account public or private. It's public by default.</p>
+        <Switch />
+      </div>
     </div>
 
-    <div className="themes">
-      <h2>THEMES</h2>
-      <p>Customize the theme of your FitCheck</p>
-      <Select options={options} />
-    </div>
-
-    <div className="privacy">
-      <h2>PRIVACY</h2>
-      <p>Make your account public or private. It's public by default.</p>
-      <Switch />
-    </div>
-
-    </>
   );
 }
 
