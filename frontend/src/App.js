c5686653d207aadb8ece9ad31c41d1f9d6333400
Andy-Nguyen4 <andynguyen1541@gmail.com>
2024-03-17 14:51:22 -0400

    deleted some files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Profile from "./pages/profile";
-import './index.css';
 import NavBar from './components/navbar';
+import './index.css';
 
 function App() {
+
   return (
 
     <div className="App">
