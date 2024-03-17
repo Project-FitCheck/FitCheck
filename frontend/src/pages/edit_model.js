eeaae3e381581144434d3c7e341222bac4739e23
Ephraim Acquah <ephraimacquah14@gmail.com>
2024-03-17 16:45:29 -0400

    added buttons and icons for each body part to edit

--- frontend/src/pages/edit_model.js
+++ frontend/src/pages/edit_model.js
@@ -14,29 +14,34 @@ function EditModel() {
                         <Button variant="contained">Female</Button>
                     </li>
                     <li className="head-setting">
+                        <div className="head-icon" />
                         <Button variant="contained">Head</Button>
                         {/*Head button that goes to page to 
                         edit head size on full model*/}
                     </li>
                     <li className="torso-setting">
+                        <div className="torso-icon" />
                         <Button variant="contained">Torso</Button>
                         {/*Torso button that goes to page to 
                         edit torso length and width while on
                         full model*/}
                     </li>
                     <li className="arms-setting">
+                        <div className="arms-icon" />
                         <Button variant="contained">Arms</Button>
                         {/*Arms button that goes to page to 
                         edit arm length and thickness while
                         on full model*/}
                     </li>
                     <li className="legs-setting">
+                        <div className="legs-icon" />
                         <Button variant="contained">Legs</Button>
                         {/*Legs button that goes to page to 
                         edit arm length and thickness while
                         on full model*/}
                     </li>
                     <li className="feet-setting">
+                        <div className="feet-icon" />
                         <Button variant="contained">Feet</Button>
                         {/*Feet button that goes to page to 
                         edit feet size on fill model*/}
