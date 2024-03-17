3079a7e40929ec0ef04eb0e7f54c178619cc10c7
Ephraim Acquah <ephraimacquah14@gmail.com>
2024-03-17 14:36:10 -0400

    started work on model edit page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './index.css';
+import './styles/index.css';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import NavBar from './components/navbar';
 
 import Profile from "./pages/profile";
+import EditModel from "./pages/edit_model";
 
 
 function App() {
@@ -13,11 +14,11 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
+          <Route path='/' element={<Navigate to="/closet" />} />
           <Route path='/closet' element={<></>} />
           <Route path='/closet/add' element={<></>} />
-          <Route path='/model/edit' element={<></>} />
+          <Route path='/model/edit' element={<EditModel />} />
           <Route path='/profile' element={<Profile />} />
-          <Route path='/' element={<Navigate to="/closet"/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/pages/edit_model.js
+++ frontend/src/pages/edit_model.js
@@ -1,9 +1,50 @@
 import React from "react";
+import "../styles/editModel.css";
+import { Button } from "@mui/base";
+import ModelViewer from "../components/model_viewer"
 
 function EditModel() {
     return (
-        <div></div>
-    )
+        <div className="EditModel">
+            <ModelViewer />
+            <div className="ModelSettings">
+                <ul>
+                    <li className="gender-setting">
+                        <Button variant="contained">Male</Button>
+                        <Button variant="contained">Female</Button>
+                    </li>
+                    <li className="head-setting">
+                        <Button variant="contained">Head</Button>
+                        {/*Head button that goes to page to 
+                        edit head size on full model*/}
+                    </li>
+                    <li className="torso-setting">
+                        <Button variant="contained">Torso</Button>
+                        {/*Torso button that goes to page to 
+                        edit torso length and width while on
+                        full model*/}
+                    </li>
+                    <li className="arms-setting">
+                        <Button variant="contained">Arms</Button>
+                        {/*Arms button that goes to page to 
+                        edit arm length and thickness while
+                        on full model*/}
+                    </li>
+                    <li className="legs-setting">
+                        <Button variant="contained">Legs</Button>
+                        {/*Legs button that goes to page to 
+                        edit arm length and thickness while
+                        on full model*/}
+                    </li>
+                    <li className="feet-setting">
+                        <Button variant="contained">Feet</Button>
+                        {/*Feet button that goes to page to 
+                        edit feet size on fill model*/}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default EditModel;
