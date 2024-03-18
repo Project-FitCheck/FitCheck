44dd221747298b2dd5df121709914331b2ec487a
Andy-Nguyen4 <andynguyen1541@gmail.com>
2024-03-18 01:47:35 -0400

    some changes to the model editor

--- frontend/src/components/modelEditorViewer.js
+++ frontend/src/components/modelEditorViewer.js
@@ -8,10 +8,10 @@ function ModelEditor() {
 
     const [imgStyle, setImgStyle] = useState({
         position: "absolute",
-        top: "0",
-        left: "0",
-        width: "100px",
-        height: "100px"
+        top: "250px",
+        left: "250px",
+        width: "250px",
+        height: "250px"
     });
 
     function slider (prop, value) {
@@ -27,20 +27,20 @@ function ModelEditor() {
 
                 {/*Male body*/}
 
-                <img src="../assets/FitCheck_male_template/male_body.svg" alt="male body" />
-                <img src="../assets/FitCheck_male_template/male_feet.svg" alt="male feet" />
-                <img src="../assets/FitCheck_male_template/male_head.svg" alt="male head" />
-                <img src="../assets/FitCheck_male_template/male_leftarm.svg" alt="male leftarm" />
-                <img src="../assets/FitCheck_male_template/male_legs.svg" alt="male legs" />
-                <img src="../assets/FitCheck_male_template/male_rightarm.svg" alt="male rightarm" />
-                <img src="../assets/FitCheck_male_template/male_torso.svg" alt="male torso" />
+                <img src="../assets/FitCheck_male_template/male_body.svg" style={imgStyle} alt="male body" />
+                <img src="../assets/FitCheck_male_template/male_feet.svg" style={imgStyle} alt="male feet" />
+                <img src="../assets/FitCheck_male_template/male_head.svg" style={imgStyle} alt="male head" />
+                <img src="../assets/FitCheck_male_template/male_leftarm.svg" style={imgStyle} alt="male leftarm" />
+                <img src="../assets/FitCheck_male_template/male_legs.svg" style={imgStyle} alt="male legs" />
+                <img src="../assets/FitCheck_male_template/male_rightarm.svg" style={imgStyle} alt="male rightarm" />
+                <img src="../assets/FitCheck_male_template/male_torso.svg" style={imgStyle} alt="male torso" />
 
             </ResponsiveSVG>
 
             {/*sliders*/}
 
             <div className="sliderX">
-                <label>Width</label>
+                <label>X Position</label>
                 <Slider
                     min={0}
                     max={500}
@@ -50,12 +50,32 @@ function ModelEditor() {
             </div>
 
             <div className="sliderY">
-            <label>Height</label>
+            <label>Y Position</label>
                 <Slider
                     min={0}
                     max={500}
                     value={parseInt(imgStyle.top)}
                     onChange={value => slider("top", value)}
+                />
+            </div>
+
+            <div className="sliderW">
+            <label>Width</label>
+                <Slider
+                    min={0}
+                    max={500}
+                    value={parseInt(imgStyle.width)}
+                    onChange={value => slider("width", value)}
+                />
+            </div>
+
+            <div className="sliderH">
+            <label>Height</label>
+                <Slider
+                    min={0}
+                    max={500}
+                    value={parseInt(imgStyle.height)}
+                    onChange={value => slider("height", value)}
                 />
             </div>
 
