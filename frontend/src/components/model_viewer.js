605d4b778e9035fc812ac8bab9ce1bc9c8575b7b
Ephraim Acquah <ephraimacquah14@gmail.com>
2024-03-17 23:41:29 -0400

    added most of the edit model page functionality

--- frontend/src/components/model_viewer.js
+++ frontend/src/components/model_viewer.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { ReactComponent as MaleModel } from "../assets/FitCheck_male_template/male_body.svg";
+import { ReactComponent as FemaleModel } from "../assets/FitCheck_female_template/female_body.svg";
 
-function ModelViewer() {
-    return (
-        <div className="ModelViewer"></div>
-    )
+function ModelViewer({model}) {
+        if (model === "male") {
+            return (<div className="ModelViewer">
+                <MaleModel />
+            </div>)}
+        else if (model === "female") {
+            return (<div className="ModelViewer">
+            <FemaleModel />
+        </div>)
+        }
+
 }
 
 export default ModelViewer;
--- frontend/src/pages/edit_model.js
+++ frontend/src/pages/edit_model.js
@@ -1,53 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/editModel.css";
 import { Button } from "@mui/base";
 import ModelViewer from "../components/model_viewer"
 
 function EditModel() {
+    const [gender, setGender] = useState(null);
+    const [active, setActive] = useState(null);
+
+    function handleClick(newGender) {
+        if (active) {
+            setActive("active");
+        } else {
+            setActive("");
+        }
+        setGender(newGender)
+    }
     return (
         <div className="EditModel">
-            <ModelViewer />
             <div className="ModelSettings">
                 <ul>
                     <li className="gender-setting">
-                        <Button variant="contained">Male</Button>
-                        <Button variant="contained">Female</Button>
+                        <Button className={active} variant="contained" onClick={() => handleClick("male")}>Male</Button>
+                        <Button className={active} variant="contained" onClick={() => handleClick("female")}>Female</Button>
                     </li>
                     <li className="head-setting">
                         <div className="head-icon" />
-                        <Button variant="contained">Head</Button>
-                        {/*Head button that goes to page to 
-                        edit head size on full model*/}
+                        <Button className={"active" ? active : ""} variant="contained">Head</Button>
                     </li>
                     <li className="torso-setting">
                         <div className="torso-icon" />
-                        <Button variant="contained">Torso</Button>
-                        {/*Torso button that goes to page to 
-                        edit torso length and width while on
-                        full model*/}
+                        <Button className={"active" ? active : ""} variant="contained">Torso</Button>
                     </li>
                     <li className="arms-setting">
                         <div className="arms-icon" />
-                        <Button variant="contained">Arms</Button>
-                        {/*Arms button that goes to page to 
-                        edit arm length and thickness while
-                        on full model*/}
+                        <Button className={"active" ? active : ""} variant="contained">Arms</Button>
                     </li>
                     <li className="legs-setting">
                         <div className="legs-icon" />
-                        <Button variant="contained">Legs</Button>
-                        {/*Legs button that goes to page to 
-                        edit arm length and thickness while
-                        on full model*/}
+                        <Button className={"active" ? active : ""} variant="contained">Legs</Button>
                     </li>
                     <li className="feet-setting">
                         <div className="feet-icon" />
-                        <Button variant="contained">Feet</Button>
-                        {/*Feet button that goes to page to 
-                        edit feet size on fill model*/}
+                        <Button className={"active" ? active : ""} variant="contained">Feet</Button>
                     </li>
                 </ul>
             </div>
+            <ModelViewer model={gender} />
+            <Button className="save-model">Save Model</Button>
         </div>
     );
 }
