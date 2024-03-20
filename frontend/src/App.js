43d56e0933046daa585f995ffbb4150aae0b8686
Ephraim Acquah <103010587+ephraimacquah@users.noreply.github.com>
2024-03-19 23:28:26 -0400

    created login page, still needs some more api func (#31)
    * created login page, still needs some more api func
    
    * fixed login page to say login
    
    * Signup page (#32)
    
    * created signup page with api functionality.
    
    to be updated to look nicer, still wip
    
    * added signup_page styling to createModel.css 4 now
    
    * fixed leak
    
    * removed unneccesary from index.js
    
    * finished login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,14 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import NavBar from './components/navbar.js';
 
+import Signup from './pages/signup';
+import Login from './pages/login';
 import Profile from "./pages/profile";
 import Closet from './pages/closet';
 import Outfits from './pages/locker';
 import AddClothing from './pages/add_clothing';
+import CreateModel from './pages/create_model';
+
 
 function App() {
   return (
@@ -17,12 +21,15 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
+          <Route path='/signup' element={<Signup />} />
+          <Route path="/login" element={<Login />} />
           <Route path='/closet' element={<Closet />} />
           <Route path='/closet/add' element={<AddClothing />} />
           <Route path='/model/edit' element={<></>} />
           <Route path='/locker' element={<Outfits />}/>
           <Route path='/profile' element={<Profile />} />
-          <Route path='/' element={<Navigate to="/closet"/>}/>
+          <Route path='/model/create' element={<CreateModel />} />
+          <Route path='/' element={<Navigate to="/signup" />} />
         </Routes>
       </BrowserRouter>
     </div>
