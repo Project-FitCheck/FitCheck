6f6cdd718e96a53252c12cfa8487faeb7486f367
Sean Mclaughlin <semcl2001@gmail.com>
2024-03-20 12:35:09 -0400

    Fixed Components folder to hold all components

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../CSS/Card.css';
+
+function Card() {
+	return <div className='Card'>
+		<a href=""><img src=""/></a>
+	</div>
+}
+
+export default Card;
---
+++ frontend/src/components/MainPage.jsx
@@ -0,0 +1,14 @@
+import '../CSS/MainPage.css';
+import PageNav from '../components/PageNav.jsx';
+import Clothes from '../components/Clothes.jsx';
+import Outfits from '../components/Outfits.jsx';
+
+
+function MainPage(props) {
+	return <div className='MainPage'>
+		<PageNav />
+		<Clothes />
+	</div>
+}
+
+export default MainPage;
