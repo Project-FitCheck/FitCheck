2eeba12f4e4504bf121e484502139cb66faedf9b
smclaughlin10 <140640503+smclaughlin10@users.noreply.github.com>
2024-03-31 18:28:02 -0400

    Model page (#42)
    * Started Model Page and fixed some css
    
    * Edit changes for alpha
    
    * Edit changes for alpha

--- frontend/src/components/model_viewer.js
+++ frontend/src/components/model_viewer.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { ReactComponent as MaleModel } from "../assets/FitCheck_male_template/male_body.svg";
 import { ReactComponent as FemaleModel } from "../assets/FitCheck_female_template/female_body.svg";
 import axios from "axios";
+import '../styles/ModelViewer.css';
+
 
 function ModelViewer({ props }) {
     const [model, setModel] = useState(null);
@@ -12,6 +14,8 @@ function ModelViewer({ props }) {
             try {
                 const userId = window.localStorage.getItem("userId");
                 const response = await axios.get("http://localhost:3001/model/?userId=" + userId);
+				response.data.fullBody.replace(/"/g, '');
+
                 setModel(response.data.fullBody);
                 setModelGender(response.data.gender);
             } catch (error) {
--- frontend/src/pages/model.js
+++ frontend/src/pages/model.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React/* , {useState} */ from 'react';
 import '../styles/Model.css';
 import ModelNav from '../components/ModelNav';
 import ModelViewer from '../components/model_viewer';
@@ -6,6 +6,7 @@ import NavBar from '../components/navbar.js';
 
 
 function Model() {
+    //const [modelData, setModelData] = useState({gender: "", head: "", torso: "", leftArm: "", righttArm: "", legs: "", feet: "", fullBody: ""});
 	return (<div className='MainPage'>
 		<div className='Model'>
 			<NavBar />
