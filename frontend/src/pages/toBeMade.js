8f21bfabeb016659c67ae3e06e9e62daaae63e0d
Andy-Nguyen4 <andynguyen1541@gmail.com>
2024-04-08 17:22:14 -0400

    started catalogue page

--- frontend/src/pages/toBeMade.js
+++ frontend/src/pages/toBeMade.js
@@ -4,7 +4,7 @@ import NavBar from '../components/navbar.js';
 function ToBeMade() {
 	return (<div className='ToBeMade'>
 		<NavBar />
-		<h1>This page has yet to be made</h1>
+		<h1>Catalogue</h1>
 	</div>);
 }
 
