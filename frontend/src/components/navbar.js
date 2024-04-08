423fb2eeec452afdd84b3160d60de773a445e87b
Ephraim Acquah <ephraimacquah14@gmail.com>
2024-04-08 18:29:16 -0400

    fixed navbar ui and leaking styles

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -10,21 +10,26 @@ const NavBar = () => {
 
     return (
         <div className="navigation">
+            <div className='logo'>
+                <img src='/fitcheck_logo_192_v2.png' alt="fitcheck logo"></img>
+                <h1>FitCheck</h1>
+            </div>
+
             <Stack spacing={2} direction="column">
                 <Button component={Link} to={"/closet"}
-                    className={location.pathname === isActive('/closet') ? 'active' : 'inactive'}
+                    className="home" {...location.pathname === (isActive('/closet') || isActive("/locker")) ? 'active' : 'inactive'}
                     variant="contained"><img src="/icons/icons8-home-logo.svg" alt="home icon"></img> Home </Button>
 
                 <Button component={Link} to={"/model"}
-                    className={location.pathname === isActive('/model') ? 'active' : 'inactive'}
+                    className="model" {...location.pathname === isActive('/model') ? 'active' : 'inactive'}
                     variant="contained"><img src="/icons/icons8-user-50.png" alt="model icon"></img> Model </Button>
 
-                <Button component={Link} to={"/search"}
-                    className={location.pathname === isActive('/search') ? 'active' : 'inactive'}
-                    variant="contained"><img src="/icons/icons8-search.svg" alt="search icon"></img> Search</Button>
+                <Button component={Link} to={"/catalog"}
+                    className="catalog" {...location.pathname === isActive('/catalog') ? 'active' : 'inactive'}
+                    variant="contained"><img src="/icons/icons8-search.svg" alt="search icon"></img> Catalog</Button>
 
                 <Button component={Link} to={"/profile"}
-                    className={location.pathname === isActive('/profile') ? 'active' : 'inactive'}
+                    className="profile" {...location.pathname === isActive('/profile') ? 'active' : 'inactive'}
                     variant="contained"><img src="/icons/icons8-cog.svg" alt="setting icon"></img> Profile</Button>
             </Stack>
         </div>
