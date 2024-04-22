517d3f5d4bfe294cd1e20ed8cea6702d779411da
Andy-Nguyen4 <andynguyen1541@gmail.com>
2024-04-21 21:03:39 -0400

    add some more colors to the header of some pages

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,7 +1,14 @@
-import { Stack } from '@mui/material';
+import { Button, Stack } from '@mui/material';
+import { Link, useLocation } from 'react-router-dom';
 import { Navb } from './navb';
 
 const NavBar = (props) => {
+
+    const location = useLocation();
+
+    const isActive = (path) => {
+        return location.pathname.startsWith(path);
+    };
 
     let home, model, catalog, profile = false;
     if (props.page === "home") {
@@ -16,17 +23,17 @@ const NavBar = (props) => {
 
     return (
         <div className="navigation">
-            
+
             <div className='logo'>
                 <img src='/fitcheck_logo_192_v2.png' alt="fitcheck logo"></img>
                 <h1>FitCheck</h1>
             </div>
 
             <Stack spacing={2} direction="column">
-                <Navb page={home} link="/closet" img="/icons/icons8-home-logo.svg" name="HOME"/>
-                <Navb page={model} link="/model" img="/icons/icons8-user-50.png" name="MODEL"/>
-                <Navb page={catalog} link="/catalog" img="/icons/icons8-search.svg" name="CATALOG"/>
-                <Navb page={profile} link="/profile" img="/icons/icons8-cog.svg" name="PROFILE"/>
+                {/*<Navb page={home} link="/closet" img="/icons/icons8-home-logo.svg" name="HOME" />
+                <Navb page={model} link="/model" img="/icons/icons8-user-50.png" name="MODEL" />
+                <Navb page={catalog} link="/catalog" img="/icons/icons8-search.svg" name="CATALOG" />
+    <Navb page={profile} link="/profile" img="/icons/icons8-cog.svg" name="PROFILE" />*/}
 
                 <Button component={Link} to={"/closet"}
                     className="home" {...location.pathname === ('/closet') ? 'active' : 'inactive'}
