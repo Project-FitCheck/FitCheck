f8bb13df9a7000b8ce205d6692931a28855cf838
smclaughlin10 <140640503+smclaughlin10@users.noreply.github.com>
2024-04-23 18:37:29 -0400

    Added AddClothing Feature, fixed side scrolling (#58)

--- frontend/src/pages/closet.js
+++ frontend/src/pages/closet.js
@@ -3,13 +3,13 @@ import PageNav from '../components/PageNav.jsx';
 import Clothes from '../components/Clothes.jsx';
 import NavBar from '../components/navbar.js';
 
-
 function Closet() {
-	return <div className='MainPage'>
-		<NavBar page="home"/>
-		<PageNav page={'clothes'} />
-		<Clothes />
-	</div>
+	return (
+		<div className='MainPage'>
+			<NavBar page="home"/>
+			<PageNav page={'clothes'} />
+			<Clothes />
+		</div>)
 }
 
 export default Closet;
